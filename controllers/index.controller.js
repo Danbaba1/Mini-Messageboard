cac7bd12322ae41f2627aea81dec647ec23a1696
Danbaba1 <oladepodaniel000@gmail.com>
2026-01-01 10:59:11 +0100

    Added new feature

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -12,12 +12,28 @@ async function newMessagePost(req, res) {
     const messageText = req.body.messageText;
     const messageUser = req.body.messageUser;
 
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    const newId = messages.length;
+
+    messages.push({ id: newId, text: messageText, user: messageUser, added: new Date() });
     res.redirect('/');
+}
+
+async function messageDetails(req, res) {
+    let id = parseInt(req.params.id);
+    const message = messages.find(m => m.id == id);
+
+    if (!message) {
+        res.status(404).send(
+            "Message not found"
+        );
+        return;
+    }
+    res.render("message-details", { title: "Mini Messageboard", message: message });
 }
 
 module.exports = {
     index,
     newMessageGet,
-    newMessagePost
+    newMessagePost,
+    messageDetails
 }
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { index, newMessageGet, newMessagePost } = require('../controllers/index.controller');
+const { index, newMessageGet, newMessagePost, messageDetails } = require('../controllers/index.controller');
 
 router.get('/', index);
 
 router.get('/new', newMessageGet);
 
 router.post('/new', newMessagePost);
+
+router.get('/messages/:id', messageDetails);
 
 module.exports = router;
